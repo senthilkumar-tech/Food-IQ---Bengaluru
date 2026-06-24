5ba2681e476c650d806e94517e980ab0d32ae64f
Senthilkumar-tech <senthilkumart2005@gmail.com>
2026-06-24 10:15:56 +0530

    fix debug route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const db = require("./db");
 
 const dashboardRoutes = require("./routes/dashboard");
 
